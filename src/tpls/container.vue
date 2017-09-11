ee8b8668ea011963bb732e253465207147dbc104
DESKTOP-9F9FSSP\budblack <budblack@qq.com>
2017-09-11 17:51:33 +0800

    init repo

---
+++ src/tpls/container.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        Container
+    </div>
+</template>
+<style>
+
+</style>
+<script>
+
+    export default {
+        data() {
+        },
+        eatch: {},
+        methods: {},
+        mounted: function () {
+            this.emit('core/module/init', this)
+        }
+    }
+
+</script>
